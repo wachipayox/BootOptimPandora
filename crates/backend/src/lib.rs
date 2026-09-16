05844e8db878e91f12e03b5d3c54204883418f27
Wachii <dani.alvarado.sosa@gmail.com>
2026-09-16 13:21:20 +0200

    backend: attach profile layout API to Instance

--- crates/backend/src/lib.rs
+++ crates/backend/src/lib.rs
@@ -29,6 +29,7 @@ mod prelaunch_attribution;
 #[cfg(test)]
 mod prelaunch_mods_probe;
 pub mod profile_layout;
+mod profile_layout_instance;
 mod server_list_pinger;
 mod shortcut;
 mod skin_manager;
