44e8a3c0ceda78cb0115abb8117c30d6466df6ee
Wachii <dani.alvarado.sosa@gmail.com>
2026-09-22 01:30:53 +0200

    agent203: wire AppCDS identity cache module

--- bootoptim-v0/interposer/src/main.rs
+++ bootoptim-v0/interposer/src/main.rs
@@ -1,4 +1,9 @@
+#[cfg(windows)]
+#[path = "../../ntfs_usn_direct.rs"]
+mod ntfs_usn_direct;
+
 include!("part1.rs");
+include!("identity_cache.rs");
 include!("part2.rs");
 include!("part3.rs");
 include!("part6.rs");
