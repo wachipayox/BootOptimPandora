8b3e2b5fecd3cc84cfa90dc62141a5f61157965a
Wachii <dani.alvarado.sosa@gmail.com>
2026-09-21 22:26:25 +0200

    Include exact-clone candidate adoption

--- bootoptim-v0/interposer/src/main.rs
+++ bootoptim-v0/interposer/src/main.rs
@@ -2,6 +2,7 @@ include!("part1.rs");
 include!("part2.rs");
 include!("part3.rs");
 include!("profile_namespace.rs");
+include!("exact_clone.rs");
 include!("part6.rs");
 include!("part4.rs");
 include!("part5.rs");
